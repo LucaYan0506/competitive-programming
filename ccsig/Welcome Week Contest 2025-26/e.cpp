91dbc7c659ed90defcde91b885da5e56744c1968
LucaYan0506 <lucayan0506@gmail.com>
2025-09-21 22:42:29 +0100

    update

---
+++ ccsig/Welcome Week Contest 2025-26/e.cpp
@@ -0,0 +1,167 @@
+#include<bits/stdc++.h>
+
+#define int long long 
+#define double long double 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORI(i, a, b) for (int i = (a); i > (b); i--)
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define oo 1000000000000000000ll
+
+const int mod = 1e9+7;
+using namespace std;
+
+random_device dev;
+mt19937 rng(dev());
+int get_random(int l, int r) {
+    return uniform_int_distribution<int>(l, r)(rng);
+}
+// uniform_int_distribution<std::mt19937::result_type> uni(1,6); // distribution in range [1, 6]
+// cout << uni(rng) << endl;
+
+int customPow (int base, unsigned int exp){
+    int res = 1;
+    while (exp) {
+        if (exp & 1)
+            res *= base;
+        exp >>= 1;
+        base *= base;
+    }
+    return res;
+}
+
+struct Point{
+    int x,y;
+    Point(){
+        x = -1;
+        y = -1;
+    }
+    Point(int first, int second){
+        x = first;
+        y = second;
+    }
+
+    bool operator<(const Point& other) const {
+        if (x != other.x) return x < other.x;
+        return y < other.y;
+    }
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+};
+
+vector<int> SieveOfEratosthenes(int n){
+    vector<bool> isPrime(n + 1, true);
+    for (int p = 2; p * p <= n; p++) 
+        if (isPrime[p] == true) 
+            for (int i = p * p; i <= n; i += p)
+                isPrime[i] = false;
+
+    vector<int> primes;
+    FOR(i,2, n + 1)
+        if (isPrime[i])
+            primes.push_back(i);
+
+    return primes;
+}
+
+pair<Point, Point> farthestManhattanPair(const vector<Point>& points) {
+    auto manhattan = [](const Point& a, const Point& b) {
+        return abs(a.x - b.x) + abs(a.y - b.y);
+    };
+
+    pair<Point, Point> bestPair = make_pair(Point(0, 0), Point(0, 0));
+    int maxDistance = -1;
+
+    // List of 4 transformations
+    vector<pair<int, int>> directions = {
+        {1, 1},
+        {1, -1},
+        {-1, 1},
+        {-1, -1}
+    };
+
+    for (auto [dx, dy] : directions) {
+        int maxVal = INT_MIN, minVal = INT_MAX;
+        Point maxPoint, minPoint;
+
+        for (const auto& [x, y] : points) {
+            int val = dx * x + dy * y;
+            if (val > maxVal) {
+                maxVal = val;
+                maxPoint = {x, y};
+            }
+            if (val < minVal) {
+                minVal = val;
+                minPoint = {x, y};
+            }
+        }
+
+        int dist = manhattan(maxPoint, minPoint);
+        if (dist > maxDistance) {
+            maxDistance = dist;
+            bestPair = make_pair(minPoint, maxPoint);
+        }
+    }
+
+    return bestPair;
+}
+
+void fastIO(){
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+}
+
+void solve(){
+    int n, e;
+    cin >> n;
+
+    vector<pair<int,int>> a(n);
+
+    for (int i=0; i<n; i++){
+        cin >> a[i].first >> a[i].second;
+    }
+
+    cin >> e;
+
+    priority_queue<array<int,3>, vector<array<int,3>>, greater<>> q;
+    for (int i=0; i<n; i++){
+        q.push({a[i].second, a[i].first, 0});
+    }
+
+    int ans = 0;
+    int days = 0;
+    while (!q.empty()){
+        auto [deadline, time, extended] = q.top();
+        q.pop();
+        if (days + time > deadline){
+            if (extended) {cout << -1 << endl; return;}
+            q.push({deadline + e, time, 1});
+        }
+        else days += time;
+    }
+    
+    cout << ans << endl;
+
+
+}
+
+int32_t main(){
+    fastIO();
+    int t = 1;
+    // cin >> t;
+    // while(t--)
+    solve();
+
+    return 0;
+}
---
+++ ccsig/Welcome Week Contest 2025-26/e2.cpp
@@ -0,0 +1,179 @@
+#include<bits/stdc++.h>
+
+#define int long long 
+#define double long double 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORI(i, a, b) for (int i = (a); i > (b); i--)
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define oo 1000000000000000000ll
+
+const int mod = 1e9+7;
+using namespace std;
+
+random_device dev;
+mt19937 rng(dev());
+int get_random(int l, int r) {
+    return uniform_int_distribution<int>(l, r)(rng);
+}
+// uniform_int_distribution<std::mt19937::result_type> uni(1,6); // distribution in range [1, 6]
+// cout << uni(rng) << endl;
+
+int customPow (int base, unsigned int exp){
+    int res = 1;
+    while (exp) {
+        if (exp & 1)
+            res *= base;
+        exp >>= 1;
+        base *= base;
+    }
+    return res;
+}
+
+struct Point{
+    int x,y;
+    Point(){
+        x = -1;
+        y = -1;
+    }
+    Point(int first, int second){
+        x = first;
+        y = second;
+    }
+
+    bool operator<(const Point& other) const {
+        if (x != other.x) return x < other.x;
+        return y < other.y;
+    }
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+};
+
+vector<int> SieveOfEratosthenes(int n){
+    vector<bool> isPrime(n + 1, true);
+    for (int p = 2; p * p <= n; p++) 
+        if (isPrime[p] == true) 
+            for (int i = p * p; i <= n; i += p)
+                isPrime[i] = false;
+
+    vector<int> primes;
+    FOR(i,2, n + 1)
+        if (isPrime[i])
+            primes.push_back(i);
+
+    return primes;
+}
+
+pair<Point, Point> farthestManhattanPair(const vector<Point>& points) {
+    auto manhattan = [](const Point& a, const Point& b) {
+        return abs(a.x - b.x) + abs(a.y - b.y);
+    };
+
+    pair<Point, Point> bestPair = make_pair(Point(0, 0), Point(0, 0));
+    int maxDistance = -1;
+
+    // List of 4 transformations
+    vector<pair<int, int>> directions = {
+        {1, 1},
+        {1, -1},
+        {-1, 1},
+        {-1, -1}
+    };
+
+    for (auto [dx, dy] : directions) {
+        int maxVal = INT_MIN, minVal = INT_MAX;
+        Point maxPoint, minPoint;
+
+        for (const auto& [x, y] : points) {
+            int val = dx * x + dy * y;
+            if (val > maxVal) {
+                maxVal = val;
+                maxPoint = {x, y};
+            }
+            if (val < minVal) {
+                minVal = val;
+                minPoint = {x, y};
+            }
+        }
+
+        int dist = manhattan(maxPoint, minPoint);
+        if (dist > maxDistance) {
+            maxDistance = dist;
+            bestPair = make_pair(minPoint, maxPoint);
+        }
+    }
+
+    return bestPair;
+}
+
+void fastIO(){
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+}
+
+static bool cmp(pair<int,int> a, pair<int,int> b){
+    return a.second < b.second;
+}
+
+bool ok(vector<pair<int,int>> vec){
+    sort(all(vec), cmp);
+    int t = 0;
+    FOR(i,0,vec.size()){
+        t += vec[i].first;
+        if (t > vec[i].second)
+            return false;
+    }
+
+    return true;
+}
+
+void solve(){
+    int n; cin >> n;
+    vector<pair<int,int>> assign(n);
+    FOR(i,0,n)
+        cin >> assign[i].first >> assign[i].second;
+    int e; cin >> e;
+
+    int res = oo;
+    FOR(mask, 0, 1 << (n+1)){
+        int cnt = 0;
+        vector<pair<int,int>> newAssign = assign;
+        
+        FOR(i,0,n){
+            int t = 1 << i;
+            if (mask&t){
+                cnt++;
+                newAssign[i].second += e;
+            }
+        }
+        
+
+        if (ok(newAssign))
+            res = min(cnt,res);
+    }
+    if (res == oo)
+        cout << -1 << endl;
+    else
+        cout << res << endl;
+}
+
+int32_t main(){
+    fastIO();
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+
+    return 0;
+}
